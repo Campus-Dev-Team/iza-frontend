a9f20ba93a613313b2723ab4e4ef4b1f58fec63c
dvn495 <152308538+dvn495@users.noreply.github.com>
2025-02-13 10:15:28 -0500

    Fix chat confundidos

--- src/components/ChatPage/ChatContainer.jsx
+++ src/components/ChatPage/ChatContainer.jsx
@@ -1,53 +1,76 @@
 import React, { useEffect, useState } from "react";
 import { MessageList } from "./MessageList";
 import { ChatInput } from "./ChatInput";
-import { LoadingMessage } from "./LoadingMessage"
+import { LoadingMessage } from "./LoadingMessage";
 import { wsService } from "../../services/wssChatService";
 import { useChat } from "../../context/ChatContext";
+import { getChatId } from "@/services/chatService";
 
 export const ChatContainer = () => {
   const { messages, setMessages, isInputEnabled } = useChat();
   const [isConnected, setIsConnected] = useState(false);
-  const [isLoading, setIsLoading] = useState(false)
+  const [chatId, setChatId] = useState();
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(() => {
+    const fetchChatId = async () => {
+      try {
+        const response = await getChatId();
+        setChatId(response.data);
+      } catch (error) {
+        console.error("Error loading chat id:", error);
+      }
+    };
+
+    fetchChatId();
+  }, []);
 
   const handleSendMessage = async (message) => {
     if (!message.trim() || !isConnected || !isInputEnabled) return;
 
-    const userName = localStorage.getItem('userName');
-    const userCity = localStorage.getItem('city');
+    const userName = localStorage.getItem("userName");
+    const userCity = localStorage.getItem("city");
 
     // Agregar mensaje del usuario al chat
-    setMessages(prev => [...prev, {
-      id: Date.now(),
-      avatar: "U",
-      message: message,
-      isAI: false,
-    }]);
+    setMessages((prev) => [
+      ...prev,
+      {
+        id: Date.now(),
+        avatar: "U",
+        message: message,
+        isAI: false,
+      },
+    ]);
 
-    setIsLoading(true)
-
-
+    setIsLoading(true);
 
     try {
       const fullMessage = {
         type: "message",
         message: `Mi nombre es: ${userName}, (NO me saludes, solo usa mi nombre)soy de la ciudad: ${userCity} y mi pregunta es: ${message}`,
         city: userCity,
-        from: "WEB"
+        from: "WEB",
+        chatId: chatId,
       };
+
+      console.log("Chat id: ", chatId);
 
       await wsService.sendMessage(fullMessage);
       return true;
     } catch (error) {
       console.error("Error al enviar mensaje:", error);
-      setIsLoading(false)
-      setMessages(prev => [...prev, {
-        id: Date.now(),
-        avatar: "C",
-        message: "Lo siento, hubo un error al enviar tu mensaje. Por favor, intenta nuevamente.",
-        isAI: true,
-        isError: true
-      }]);
+      setIsLoading(false);
+      setMessages((prev) => [
+        ...prev,
+        {
+          id: Date.now(),
+          avatar: "C",
+          message:
+            "Lo siento, hubo un error al enviar tu mensaje. Por favor, intenta nuevamente.",
+          isAI: true,
+          isError: true,
+        },
+      ]);
     }
   };
 
@@ -61,10 +84,10 @@ export const ChatContainer = () => {
 
     const handleMessage = (data) => {
       try {
-        const messageContent = typeof data === "string" ? data : data.message
+        const messageContent = typeof data === "string" ? data : data.message;
 
         setTimeout(() => {
-          setIsLoading(false)
+          setIsLoading(false);
           setMessages((prev) => [
             ...prev,
             {
@@ -73,11 +96,11 @@ export const ChatContainer = () => {
               message: messageContent,
               isAI: true,
             },
-          ])
-        }, 1500) // 1.5 segundos de delay
+          ]);
+        }, 1500); // 1.5 segundos de delay
       } catch (error) {
-        console.error("Error al procesar mensaje:", error)
-        setIsLoading(false)
+        console.error("Error al procesar mensaje:", error);
+        setIsLoading(false);
       }
     };
 
